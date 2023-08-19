f1b86868a015ee6ea27a90adcc4d05103d10d975
Veronika Aniskova <v9037@yandex.ru>
2023-08-19 21:45:35 +0300

    Diploma: port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const routes = require('./routes/index');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(express.json());
@@ -29,7 +29,7 @@ const limiter = rateLimit({
 app.use(cors({
   origin: [
     'http://localhost:3001',
-    'https://mestogallery.nomoreparties.co',
+    'https://yourmovies.nomoreparties.co',
 
   ],
   credentials: true, // разрешить куки
